25061aa810e768a64f15b73e0b8be6334cb0a96d
Paul Bakker <pbakker@servoy.com>
2012-09-13 13:06:58 +0000

    Fixed usage of AbstractModuleDef for svyApplicationCore

--- svyApplicationCore/forms/svyAppCoreModDef.js
+++ svyApplicationCore/forms/svyAppCoreModDef.js
@@ -2,12 +2,6 @@
  * @properties={typeid:24,uuid:"66537566-4F88-4B80-876B-F969D750DD5B"}
  */
 function moduleInit() {
-	
-}
-
-/**
- * @properties={typeid:24,uuid:"F58B10C2-0C89-453B-98BD-D4EF96E96907"}
- */
-function getVersion() {
-	return 0.1;
+	version = 0.1;
+	id = 'svyApplicationCore'
 }
