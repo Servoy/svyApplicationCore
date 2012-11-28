c1b39f1471f545dacd10e387ba99299fe67628af
Sean Devlin <sdevlin@servoy.com>
2012-11-28 15:14:33 +0000

    Fix warnings on svyBase

--- svyApplicationCore/forms/svyBase.js
+++ svyApplicationCore/forms/svyBase.js
@@ -19,14 +19,14 @@ function onRecordSelection(event) {
  * This base implementation does nothing to the form, but does iterate over visible forms in tabpanels and split panes calling their updateUI implementations,
  * ...which if inherited from this form will be recursive to include all visible forms beneath the form which was called.
  * 
- * TODO: The recursive invocation through form-in-tab/split panels can cause unnecessary calls to updateUI. We should investigate ways to reduce to one call per thread, perhaps using continuations to defer to the end?
+ * TODO: The recursive invocation through form-in-tab/split panels can cause unnecessary calls to updateUI. 
+ * We should investigate ways to reduce to one call per thread, perhaps using continuations to defer to the end?
  * 
  * @author Sean
  * @since 2012.10.03
  * @properties={typeid:24,uuid:"811259CD-C45B-495E-9D64-2A9F3D97F038"}
  */
 function updateUI(){
-	// TODO: Invoke in sub forms
 	if(!this['elements']) return;													//	Sometimes elements is undefined when changes broadcast from developer
 	
 	var form;
@@ -35,16 +35,20 @@ function updateUI(){
 		var e = elements[i]	
 		var type = (e.getElementType) ? e.getElementType() : null;					//	get the element type
 		if(type == ELEMENT_TYPES.TABPANEL){											//	operating only on tab panels
-			form = forms[e.getTabFormNameAt(e.tabIndex)];							//	get the selected tab form
+			/** @type {RuntimeTabPanel} */
+			var tabPanel = e;
+			form = forms[tabPanel.getTabFormNameAt(tabPanel.tabIndex)];				//	get the selected tab form
 			if(form && form[updateUIMethod]){										//	update its UI
 				form[updateUIMethod]();
 			}
 		} else if(type == ELEMENT_TYPES.SPLITPANE){									//	operate only on split pane
-			form = e.getLeftForm();													//	update left form UI
+			/** @type {RuntimeSplitPane} */
+			var splitPane = e;
+			form = splitPane.getLeftForm();											//	update left form UI
 			if(form && form[updateUIMethod]){
 				form[updateUIMethod]();
 			}
-			form = e.getRightForm();												//	update right form UI
+			form = splitPane.getRightForm();										//	update right form UI
 			if(form && form[updateUIMethod]){
 				form[updateUIMethod]();
 			}
