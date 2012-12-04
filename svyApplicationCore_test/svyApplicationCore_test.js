99d20d61c2856e61b0123f6237fe7fa420036d82
Paul Bakker <pbakker@servoy.com>
2012-12-04 14:01:11 +0000

    Test code for svyApplicationCore

---
+++ svyApplicationCore_test/svyApplicationCore_test.js
@@ -0,0 +1,13 @@
+/**
+ * @properties={typeid:24,uuid:"237B9F77-817F-4846-9FBA-0953BB260999"}
+ */
+function testModuleDefinitions() {
+	//Test for not overriding final methods
+	//Test for implementing mandatory methods
+	
+	var mods = scopes.svyUtils.getJSFormInstances(solutionModel.getForm('AbstractModuleDef')) 
+
+	for (var i = 0; i < mods.length; i++) {
+		mods[i].getMethods(false).indexOf('')
+	}
+}
