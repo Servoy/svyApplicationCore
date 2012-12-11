816e3275730110643aefab61774bc07bf265703a
Paul Bakker <pbakker@servoy.com>
2012-12-11 10:19:30 +0000

    Fixed testcase for scopes.svyApplicationCore.addDataBroadcastListener()

--- svyApplicationCore_test/svyApplicationCore_test.js
+++ svyApplicationCore_test/svyApplicationCore_test.js
@@ -12,25 +12,19 @@ function testModuleDefinitions() {
 	}
 }
 
-/**
- * @properties={typeid:24,uuid:"EA781DF8-997D-44EE-ABC9-61451665A260"}
+/*
+ * Start testcode for {@link #scopes#svyApplicationCore#addDataBroadcastListener()}
  */
-function setUp() {
-	
-	
-}
-
 /**
- * TODO generated, please specify type and doc for the params
- * @param args
+ * Stores a reference to a JSFoundSet on the svy_framework/log table, so the test client received incoming databroadcasts for this table
+ * @type {JSFoundSet}
  *
- * @properties={typeid:24,uuid:"4B373614-4474-4CB4-B5D5-0B0A87E727C6"}
+ * @properties={typeid:35,uuid:"0AF38408-8622-4DE8-836D-3E516406FF67",variableType:-4}
  */
-function fireDataBroadcastNotificicationCallbackHandler(args) {
-	
-}
+var emptyFSReferenceForDatabroadcast
 
 /**
+ * Method called in HC to fire a dataChange Notification
  * @properties={typeid:24,uuid:"47E0B392-B155-4BD1-8CCA-6045F3DA8153"}
  */
 function fireDataBroadcastNotificication() {
@@ -39,12 +33,18 @@ function fireDataBroadcastNotificication() {
 }
 
 /**
+ * Dummy method needed for HC.queueMethod
+ *
+ * @properties={typeid:24,uuid:"4B373614-4474-4CB4-B5D5-0B0A87E727C6"}
+ */
+function fireDataBroadcastNotificationCallbackHandler() {}
+
+/**
  * @properties={typeid:35,uuid:"82854030-DEFD-4BFA-BEEB-A5F848B31753",variableType:-4}
  */
 var fired = false
 
 /**
- * @param {JSEvent} [event]
  * @param {String} dataSource
  * @param {Number} action
  * @param {JSDataSet} pks
@@ -52,9 +52,7 @@ var fired = false
  *
  * @properties={typeid:24,uuid:"CBD14D84-7D39-4B4B-98A6-C619DF732E5C"}
  */
-
-function dataBroadcastEventHandler(event, dataSource, action, pks, cached) {
-	application.output(arguments)
+function dataBroadcastEventHandler(dataSource, action, pks, cached) {
 	if (dataSource == 'db:/svy_framework/log' &&
 		action == SQL_ACTION_TYPES.UPDATE_ACTION &&
 		pks.getValue(1,1) == -10) {
@@ -70,20 +68,20 @@ function testDataBroadcastListener() {
 }
 
 /**
- * @properties={typeid:24,uuid:"F9D47B83-7B9E-4EDC-B4E4-C5ACEE327254"}
- */
-function tearDown() {
-	
-}
-/**
  * Callback method for when solution is opened.
  *
  * @properties={typeid:24,uuid:"9B16F740-0A85-4CA9-9F4F-91B5100D3080"}
  */
 function onSolutionOpen() {
-	application.output('open')
-	scopes.svyApplicationCore.addDataBroadcastListener(dataBroadcastEventHandler)
-	var client = plugins.headlessclient.createClient(application.getSolutionName(),null,null,null)
-	client.queueMethod('scopes.svyApplicationCore_test','fireDataBroadcastNotificication', null, fireDataBroadcastNotificicationCallbackHandler)
-	application.sleep(2000)
+	if (application.getApplicationType() != APPLICATION_TYPES.HEADLESS_CLIENT) { //So code is only executed when it's in the Test Client
+		scopes.svyApplicationCore.addDataBroadcastListener(dataBroadcastEventHandler)
+		emptyFSReferenceForDatabroadcast = databaseManager.getFoundSet('db:/svy_framework/log')
+		emptyFSReferenceForDatabroadcast.clear()
+		var client = plugins.headlessclient.createClient(application.getSolutionName(),null,null,null)
+		client.queueMethod('scopes.svyApplicationCore_test','fireDataBroadcastNotificication', null, fireDataBroadcastNotificationCallbackHandler)
+	}
 }
+
+/*
+ * End testcode for {@link #scopes#svyApplicationCore#addDataBroadcastListener()}
+ */
