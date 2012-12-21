66b422b8397d1b497fbdb5992a6772d876a67da2
Patrick Ruhsert <pruhsert@servoy.com>
2012-12-21 12:55:50 +0000

    added a dummy variable to make sure this gets exported

--- svyApplicationCore/svyNavigationManager.js
+++ svyApplicationCore/svyNavigationManager.js
@@ -1,3 +1,11 @@
 /*
  * scope to move all the navigation logic to
  */
+/**
+ * Just to make sure the scope gets exported; can be removed when this is filled with life
+ * 
+ * @type {String}
+ *
+ * @properties={typeid:35,uuid:"067C18BD-71F1-4CEE-B491-1545B31CEBE7"}
+ */
+var dummy;
